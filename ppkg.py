319ed2866e180688734ef15d9804c3e382732faf
Jayashree Pradhan <mrbob6645@gmail.com>
2020-01-02 22:20:50 -0500

    works with any json

--- ppkg.py
+++ ppkg.py
@@ -1,9 +1,19 @@
 import json
 import os
+import argparse
+import sys
 
-pkg = json.load(open("neofetch.json"));
+
 home = os.path.expanduser("~")
 command = "export PATH=$PATH':$HOME/bin'"
+
+parser = argparse.ArgumentParser(description="Package manager for BASH")
+
+parser.add_argument('JSONpath', metavar="path of JSON file", type=str, help="path of the JSON file");
+
+args = parser.parse_args();
+pkg = json.load(open(args.JSONpath));
+
 if(os.path.isdir(home + "/bin")):
     os.system(command)
     os.system("echo " + command + " >> ~/.bash_profile")
